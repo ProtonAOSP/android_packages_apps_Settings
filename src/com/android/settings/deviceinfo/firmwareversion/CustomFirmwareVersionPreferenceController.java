6cf3d90d2627b7c66f30b6d4fe271c251099d56d
Danny Lin <danny@kdrag0n.dev>
2020-11-09 14:02:27 -0800

    Settings: Navigate to URI on custom ROM version select
    This lets us make the custom ROM version item selectable so it fits in
    at the top of the about menu. It's also better for users to discover the
    source of the ROM.
    
    While we're at it, move the item below the security patch level so that
    all of the core Android items are at the top.
    
    Change-Id: I6b6d3137ffec7495607967ab690fcb09237a59c2

--- src/com/android/settings/deviceinfo/firmwareversion/CustomFirmwareVersionPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/CustomFirmwareVersionPreferenceController.java
@@ -17,17 +17,28 @@
 package com.android.settings.deviceinfo.firmwareversion;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.SystemProperties;
+import android.text.TextUtils;
+import android.util.Log;
+
+import androidx.preference.Preference;
 
 import com.android.settings.R;
 import com.android.settings.core.BasePreferenceController;
 
 public class CustomFirmwareVersionPreferenceController extends BasePreferenceController {
 
+    private static final String TAG = "CustomFirmwareVersion";
     private static final String VERSION_PROPERTY = "ro.build.version.custom";
+
+    private final PackageManager mPackageManager;
 
     public CustomFirmwareVersionPreferenceController(Context context, String preferenceKey) {
         super(context, preferenceKey);
+        mPackageManager = mContext.getPackageManager();
     }
 
     @Override
@@ -39,5 +50,24 @@ public class CustomFirmwareVersionPreferenceController extends BasePreferenceCon
     public CharSequence getSummary() {
         return SystemProperties.get(VERSION_PROPERTY,
                 mContext.getString(R.string.device_info_default));
+    }
+
+    @Override
+    public boolean handlePreferenceTreeClick(Preference preference) {
+        if (!TextUtils.equals(preference.getKey(), getPreferenceKey())) {
+            return false;
+        }
+
+        final Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(mContext.getString(R.string.custom_firmware_uri)));
+        if (mPackageManager.queryIntentActivities(intent, 0).isEmpty()) {
+            // Don't send out the intent to stop crash
+            Log.w(TAG, "queryIntentActivities() returns empty");
+            return true;
+        }
+
+        mContext.startActivity(intent);
+        return true;
     }
 }
